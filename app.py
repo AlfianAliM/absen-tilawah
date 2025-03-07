24cd9faa7a5e05bdf43abfa0153cef53833048d0
Alfian Ali Murtadlo <alfian.ali@alumni.ui.ac.id>
2025-03-07 16:35:59 +0700

    commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import asyncio
+from telegram import Bot
+from ummalqura.hijri_date import HijriDate 
+from datetime import date
+
+TOKEN = "6894579573:AAHbFFEG3E566Gp-KWS5ItUVzpa-N-NZizM"
+CHAT_ID = "-4677671509"
+
+async def send_poll():
+    bot = Bot(token=TOKEN)
+
+    # Dapatkan tanggal Hijriah hari ini
+    today_gregorian = date.today()
+    hijri_date = HijriDate(today_gregorian.year, today_gregorian.month, today_gregorian.day, gr=True)
+    hijri_today = f"{hijri_date.day} {hijri_date.month_name} {hijri_date.year} H"
+
+    # Kirim polling
+    await bot.send_poll(
+        chat_id=CHAT_ID,
+        question=f"ABSENSI ({hijri_today}) - Sudah tilawah berapa juz?",
+        options=["1 Juz", "2 Juz", "3 Juz", "4 Juz", "5 Juz"],
+        is_anonymous=False
+    )
+
+if __name__ == "__main__":
+    asyncio.run(send_poll())  
