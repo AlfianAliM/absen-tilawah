c83cf111458e123df47f5738c4f2c2413e34819d
Alfian Ali Murtadlo <alfian.ali@alumni.ui.ac.id>
2025-03-07 16:46:45 +0700

    push

--- app.py
+++ app.py
@@ -10,18 +10,19 @@ THREAD_ID = 8961
 async def send_poll():
     bot = Bot(token=TOKEN)
 
-    # Dapatkan tanggal Hijriah hari ini
+    
     today_gregorian = date.today()
     hijri_date = HijriDate(today_gregorian.year, today_gregorian.month, today_gregorian.day, gr=True)
     hijri_today = f"{hijri_date.day} {hijri_date.month_name} {hijri_date.year} H"
 
-    # Kirim polling
+    
     await bot.send_poll(
         chat_id=CHAT_ID,
+        message_thread_id=THREAD_ID, 
         question=f"ABSENSI ({hijri_today}) - Sudah tilawah berapa juz?",
         options=["1 Juz", "2 Juz", "3 Juz", "4 Juz", "5 Juz"],
         is_anonymous=False
     )
 
 if __name__ == "__main__":
-    asyncio.run(send_poll())  
+    asyncio.run(send_poll())
